864fc4d08fd2d720c014e2df465e5fe5073181e9
daeho kim <daeho.kim@softwareinlife.com>
2021-07-15 19:47:58 +0900

    first commit

---
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -0,0 +1,16 @@
+package com.example.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// Server의 자격으로 등록해주는 annotation
+@EnableEurekaServer
+public class EcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
+
+}
