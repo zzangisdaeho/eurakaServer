2de472d679e91e51ac6ba2c18564b5540dbde495
daeho kim <daeho.kim@softwareinlife.com>
2021-07-15 20:16:55 +0900

    projectname changed

--- src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
+++ src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.ecommerce;
+package com.example.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,10 +7,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 // Server의 자격으로 등록해주는 annotation
 @EnableEurekaServer
-public class EcommerceApplication {
+public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EcommerceApplication.class, args);
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 
 }
